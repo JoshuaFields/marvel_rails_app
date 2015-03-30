3c73d564669f43a7561ee64af653aac413c99ada
Josh Fields <jfields85@gmail.com>
2015-03-30 17:12:46 -0400

    Add select menu to index

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+
+  # Doctor Doom and Deadpool have no bio, but a link to a wiki about him is returned
+  # towards the end of the JSON. Try to extract that info.
+  # Also, fix the %20 formatting, possibly with gsub
+    # might be easier to add the %20 in when a hero/villain is selected
+  # Doctor%20Doom
+  # Spider-Man
+
+  HEROES = ["Wolverine", "Hulk", "Deadpool", "Spider-Man", "Doctor%20Doom"]
+
 end
