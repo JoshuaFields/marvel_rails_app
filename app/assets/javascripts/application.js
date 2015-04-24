5bb9cb98a993155718f7d8d829fee0001a9d3ba7
Josh Fields <jfields85@gmail.com>
2015-04-24 15:00:31 -0400

    Move javascript in application.js to new file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,15 +14,15 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(document).ready(function() {
-  if ($('.pagination').length) {
-    $(window).scroll(function() {
-      var url = $('.pagination .next_page').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text("Please Wait...");
-        return $.getScript(url);
-      }
-    });
-    return $(window).scroll();
-  }
-});
+// $(document).ready(function() {
+//   if ($('.pagination').length) {
+//     $(window).scroll(function() {
+//       var url = $('.pagination .next_page').attr('href');
+//       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+//         $('.pagination').text("Please Wait...");
+//         return $.getScript(url);
+//       }
+//     });
+//     return $(window).scroll();
+//   }
+// });
